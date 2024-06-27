d920f5ea65c8526cfb58fa76c6a4721faf051e74
Tanya Agarwal <41121573+tngrwl@users.noreply.github.com>
2024-06-27 22:42:34 +0530

    Code for users list fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Table } from 'antd';
 
 function App() {
+  const [users,setUsers] = useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:3001/api/get-users")
+    .then((res) => res.json())
+    .then(result =>{
+      setUsers(result.data);
+    })
+  },[])
+
+  const columns = [{ title: 'First name',
+  dataIndex: 'firstname',
+  key: 'firstname'},{
+    title: 'Last name',
+    dataIndex: 'lastname',
+    key: 'lastname'
+  },{
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email'
+  }]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>User list</h2>
+     <Table dataSource={users} columns={columns}/>
     </div>
   );
 }
